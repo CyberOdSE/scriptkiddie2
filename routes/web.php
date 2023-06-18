ce8dba21dcb1d96e3af5ed7f345669c4fc5e8674
CyberODSE <odin@saintly.com>
2023-06-18 22:19:20 +0200

    first and last commit
    assignment done; specific routes can be accessed only through login. register option is also there and remember me button too.

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\ObservationController;
+use App\Http\Controllers\PostController;
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // Take the 3 newest posts
+    $latestPosts = Post::orderBy('published_at', 'desc')->take(3)->get();
+
+    return view('welcome', compact('latestPosts'));
+})->name('home');
+
+
+// Resource routes of the base pages.
+Route::resource('/posts', PostController::class);
+Route::resource('/observations', ObservationController::class);
+Route::resource('/locations', LocationController::class);
+Route::get('/trigger-500-error', function () { abort(500); });
+Route::resource('/locations/contact', LocationController::class);
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// User registration routes
+Route::get('/register', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('/register', 'App\Http\Controllers\Auth\RegisterController@register');
+
